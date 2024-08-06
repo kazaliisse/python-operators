c2cdac5484ffa4fa09843301a6b9d8a5feb964c8
noor <noorhafowbare@gmail.com>
2024-08-06 21:11:29 +0300

    initial commit

---
+++ operators.py
@@ -0,0 +1,10 @@
+meaning = 42
+print('')
+
+# if meaning > 10:
+#   print("yes you are right")
+# else:
+#   print("you are wrong")
+
+#Ternary operator
+print("you are right") if meaning > 10 else print("you are wrong")
